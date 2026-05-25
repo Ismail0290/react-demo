f50f5b912f3b0ad16b5f2f17851f508d0886dad1
Ismail0290 <sheikhij@rknec.edu>
2026-05-25 11:22:06 +0530

    Done basic assignment

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,13 +1,19 @@
 import Button from "./components/Button";
 const name = "ismail";
+const isStudent = true;
+const num = 7;
+const hasFess = true;
 function App() {
   
   return (
-    <div>
+    <>
       App
       <Button />
-      <p>Hello, {name.toUpperCase()} </p>
-    </div>
+      <h2>{name.toUpperCase()} </h2>
+      <p>{num}</p>
+      {isStudent ? <p>Currently Studying</p> : <p>Working Professional</p>}
+      {hasFess ? <button>Pay Fees</button> : <p></p>}
+    </>
     // JSX expression 
     
   );
