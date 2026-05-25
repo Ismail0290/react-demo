81e8e398215e1fa1f05163bb6441d24c027c30be
Ismail0290 <sheikhij@rknec.edu>
2026-05-25 11:12:16 +0530

    added initial files

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button";
+const name = "ismail";
+function App() {
+  
+  return (
+    <div>
+      App
+      <Button />
+      <p>Hello, {name.toUpperCase()} </p>
+    </div>
+    // JSX expression 
+    
+  );
+}
+
+export default App;
